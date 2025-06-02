59cc0a5aec68190bbb45dc5fe378df8b337613cf
Ayumi-495 <ayumi.mizuno5@gmail.com>
2025-06-02 13:36:48 -0600

    Update moura2021.R

--- R/moura2021.R
+++ R/moura2021.R
@@ -3,7 +3,7 @@ pacman::p_load(brms, MCMCglmm, metafor, metadat, tidyverse,
                dplyr, tidyr, purrr, stringr, readr, magrittr, 
                janitor, flextable, kableExtra, geosphere, here, rotl)
 
-# Use the data from the metadat package 
+# Use the data from the metadat package ----
 dat <- dat.moura2021$dat
 head(dat)
 
@@ -15,7 +15,7 @@ A_BM <- vcv(tree, corr=TRUE)
 dat$phy <- dat$species.id
 summary(dat)
 
-### fit BM model
+# fit BM model ----
 mod_BM <- rma.mv(yi, vi,
                  random = list(~ 1 | study.id, 
                                ~ 1 | effect.size.id, 
@@ -25,7 +25,7 @@ mod_BM <- rma.mv(yi, vi,
                   data = dat)
 summary(mod_BM)
 
-### spatial model (exponential)
+# spatial model (exponential) ----
 dat$const <- 1　# add a constant column for the spatial model
 
 A_BM[1:5, 1:5]
@@ -45,28 +45,29 @@ mod_spatial <- rma.mv(yi, vi,
                       data = dat)
 summary(mod_spatial)
 
-## OU model
-alpha <- 1/0.0182　# alpha = 1/rho - rho = 0.0182
-Tmat <- cophenetic(tree) #computes the pairwise distances between the pairs of tips from a phylogenetic tree using its branch lengths.
-format_back <- function(x) {
-  x <- tolower(x)
-  x <- gsub(" ", "_", x)
-  return(x)
-}
-rownames(Tmat) <- format_back(rownames(Tmat))
-colnames(Tmat) <- format_back(colnames(Tmat))
-Tmat <- Tmat[rownames(A_BM), colnames(A_BM)]
-
-A_OU <- exp(-alpha * Tmat)
-
+# OU model ----
+# Tmat <- cophenetic(tree) #computes the pairwise distances between the pairs of tips from a phylogenetic tree using its branch lengths.
+rho <- mod_spatial$rho
+A_OU2 <- exp(-D / rho)
 mod_OU <- rma.mv(yi, vi,
+                 random = list(~ 1 | study.id, 
+                               ~ 1 | effect.size.id, 
+                               ~ 1 | species.id, 
+                               ~ 1 | phy),
+                 R = list(phy = A_OU2), 
+                 data = dat)
+summary(mod_OU)
+## -- ##
+alpha <- 1/rho
+A_OU <- exp(-alpha * D)
+mod_OU2 <- rma.mv(yi, vi,
                  random = list(~ 1 | study.id, 
                                ~ 1 | effect.size.id, 
                                ~ 1 | species.id, 
                                ~ 1 | phy),
                  R = list(phy = A_OU), 
                  data = dat)
-summary(mod_OU)
+summary(mod_OU2)
 
 # spatial model (gaussian)
 mod_gau <- rma.mv(yi, vi,
@@ -79,3 +80,38 @@ mod_gau <- rma.mv(yi, vi,
                   control=list(rho.init=1), 
                   data = dat)
 summary(mod_gau)
+max(D)
+# linear model
+system.time(mod_linear <- rma.mv(yi, vi,
+                      random = list(~ 1 | study.id, 
+                                    ~ 1 | effect.size.id, 
+                                    ~ 1 | species.id, 
+                                    ~ phy|const),
+                      dist=list(D),
+                      struct = "SPLIN",
+                      control=list(rho.init=1 
+                                   # optimizer = "optim"
+                                   ), # Optimizer (nlminb) did not achieve convergence (convergence = 1).
+                      data = dat)
+)
+summary(mod_linear)
+
+# gaussian process model in brms ----
+formula_1 <- bf(yi | se(vi) ~ 1 + gp(D, cov = "exp_quad", gr = TRUE))
+
+prior_ma1 <- get_prior(
+  formula = formula_1,
+  data = dat,
+  data2 = list(D = D),
+  family = gaussian()
+)
+
+system.time(fit_brm1 <- brm(
+  formula_1,
+  data = dat,
+  data2 = list(D = D),
+  chains = 2, 
+  iter = 3000, 
+  warmup = 2000,
+  prior = prior_ma1
+))
