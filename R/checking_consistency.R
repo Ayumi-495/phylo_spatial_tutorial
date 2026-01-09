322f67991d8e81c256520270e9af8f07720ffb7a
Ayumi-495 <ayumi.mizuno5@gmail.com>
2026-01-09 10:15:50 -0700

    update

--- R/checking_consistency.R
+++ R/checking_consistency.R
@@ -147,8 +147,8 @@ saveRDS(sp_eg1_exp_mf, here("Rdata", "tutorial_v2", "sp_eg1_exp_brms.rds"))
 
 ### glmmTMB ----
 id_levels <- levels(dat_Roger$effect_id)
-
-VCV <- diag(dat_Roger$var_Hedges)
+dat_Roger$pos <- numFactor(dat_Roger$x_km, dat_Roger$y_km)
+VCV <- diag(dat_Roger$var_Hedges) 
 rownames(VCV) <- colnames(VCV) <- as.character(dat_Roger$effect_id)
 
 stopifnot(setequal(rownames(VCV), id_levels))
@@ -168,6 +168,75 @@ system.time(
 )
 
 summary(sp_eg1_exp_tmb)　
+
+#  Family: gaussian  ( identity )
+# Formula:          
+# d_Hedges ~ 1 + equalto(0 + effect_id | const, VCV) + exp(pos +      0 | const)
+# Data: dat_Roger
+
+#       AIC       BIC    logLik -2*log(L)  df.resid 
+#    8084.7    8107.8   -4038.4    8076.7      2357 
+
+# Random effects:
+
+# Conditional model:
+#  Groups   Name                                     Variance  Std.Dev. Corr     
+#  const    effect_id1                                0.202208 0.44968           
+#           effect_id2                                0.166676 0.40826  0.00     
+#           effect_id3                                0.225770 0.47515  0.00 0.00
+#           effect_id4                                0.197994 0.44497  0.00 0.00
+#           effect_id5                                3.676968 1.91754  0.00 0.00
+#           effect_id6                                0.315616 0.56180  0.00 0.00
+#           effect_id7                                0.305460 0.55268  0.00 0.00
+#           effect_id8                                0.313426 0.55984  0.00 0.00
+#           effect_id9                                0.413540 0.64307  0.00 0.00
+#           effect_id10                               0.325726 0.57072  0.00 0.00
+#           ...                                      ...       ...      ...  ... 
+#  const.1  pos(16262.6644099893,-5204.51951303405)   1.232997 1.11040           
+#           pos(16280.4755285163,-5116.14629455727)   1.232997 1.11040  0.00     
+#           pos(-7965.96086752978,-5019.4733863405)   1.232997 1.11040  0.00 0.00
+#           pos(-7959.34359171906,-4693.06364429579)  1.232997 1.11040  0.00 0.00
+#           pos(-7960.45678662699,-4691.63535918108)  1.232997 1.11040  0.00 0.00
+#           pos(-7096.61753807119,-4685.92422115392)  1.232997 1.11040  0.00 0.00
+#           pos(-7992.73943895704,-4607.717759142)    1.232997 1.11040  0.00 0.00
+#           pos(-7903.68384632242,-4579.4258128701)   1.232997 1.11040  0.00 0.00
+#           pos(16237.0609271069,-4578.0132445546)    1.232997 1.11040  0.00 0.00
+#           pos(16304.594788628,-4572.36489569299)    1.232997 1.11040  0.00 0.00
+#           ...                                      ...       ...      ...  ... 
+#  Residual                                           0.793492 0.89078           
+                                       
+                                       
+                                       
+                                       
+#  0.00                                  
+#  0.00 0.00                             
+#  0.00 0.00 0.00                        
+#  0.00 0.00 0.00 0.00                   
+#  0.00 0.00 0.00 0.00 0.00              
+#  0.00 0.00 0.00 0.00 0.00 0.00         
+#  0.00 0.00 0.00 0.00 0.00 0.00 0.00    
+#  ...  ...  ...  ...  ...  ...  ...  ...
+                                       
+                                       
+                                       
+#  0.00                                  
+#  0.00 0.00                             
+#  0.00 0.00 0.00                        
+#  0.00 0.00 0.00 0.00                   
+#  0.00 0.00 0.00 0.00 0.00              
+#  0.00 0.00 0.00 0.00 0.00 0.00         
+#  0.00 0.00 0.00 0.00 0.00 0.00 0.00    
+#  ...  ...  ...  ...  ...  ...  ...  ...
+                                       
+# Number of obs: 2361, groups:  const, 1
+
+# Dispersion estimate for gaussian family (sigma^2): 0.793 
+
+# Conditional model:
+#             Estimate Std. Error z value Pr(>|z|)    
+# (Intercept) -0.33493    0.06444  -5.197 2.02e-07 ***
+
+ head(confint(sp_eg1_exp_tmb), 10)
 # 2.5 %     97.5 %   Estimate
 # (Intercept)                                              -0.461234 -0.2086251 -0.3349296
 # Std.Dev.pos(16262.6644099893,-5204.51951303405)|const.1   1.007168  1.2242221  1.1104039
@@ -198,6 +267,23 @@ diagnose(sp_eg1_exp_tmb)
 # Large Z-statistics (estimate/std err) suggest a *possible* failure of the Wald approximation - often also
 # associated with parameters that are at or near the edge of their range (e.g. random-effects standard deviations  approaching 0).  
 # (Alternately, they may simply represent very well-estimated parameters; intercepts of non-centered models may fall in this category.) While the Wald p-values and standard errors listed in summary() may be unreliable, profile confidence intervals (see ?confint.glmmTMB) and likelihood ratio test p-values derived by comparing models (e.g. ?drop1) are probably still OK.  (Note that the LRT is conservative when the null value is on the boundary, e.g. a variance or zero-inflation value of 0 (Self and Liang 1987; Stram and Lee 1994; Goldman and Whelan 2000); in simple cases these p-values are approximately twice as large as they should be.)
+
 library(DHARMa)
 res = simulateResiduals(sp_eg1_exp_tmb)
 plot(res, rank = T)
+
+testUniformity(res)
+testDispersion(res)
+testOutliers(res)
+
+# 2) conditional: fitted values
+plotResiduals(res, form = predict(sp_eg1_exp_tmb))
+
+# 3) conditional: sampling variance
+plotResiduals(res, form = dat_Roger$var_Hedges)
+
+# 4) group-wise: study ID 
+plotResiduals(res, form = dat_Roger$study_id)  
+
+# 5) spatial autocorrelation
+testSpatialAutocorrelation(res, x = dat_Roger$x_km, y = dat_Roger$y_km)
